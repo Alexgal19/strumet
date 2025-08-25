70359e97986db945f94201827fc4422b89947a29
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-08-25 20:15:05 +0000

    Проблема

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { ConfigProvider } from '@/context/config-context';
-import MainLayout from '@/components/main-layout';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <ConfigProvider>
-      <MainLayout />
+      {children}
     </ConfigProvider>
   );
 }
