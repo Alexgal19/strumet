6ef6dc714f185ce1307dc5d941ffa2bdd333a08b
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-09-12 07:58:33 +0000

    що це таке? поверни все назад

--- src/components/circulation-card-print-form.tsx
+++
@@ -1,9 +0,0 @@
-// This component is no longer used for printing directly.
-// The new print page is at src/app/(app)/karty-obiegowe/druk/page.tsx.
-// This can be removed in the future if a preview is not needed.
-'use client';
-import React from 'react';
-
-export const CirculationCardPrintForm = () => {
-    return null;
-}
