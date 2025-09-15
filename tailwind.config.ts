1dba922c2eaff0021756edf438e4e36d71bbdf61
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-09-15 10:12:44 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -91,8 +91,8 @@ export default {
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.01s ease-out",
-        "accordion-up": "accordion-up 0.01s ease-out",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
         "animate-in": "animate-in 0.01s ease-out",
         "animate-out": "animate-out 0.01s ease-in",
       },
