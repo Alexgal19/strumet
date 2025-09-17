f4d9485bf79908519102fa3eca9c5b2ec3eee767
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-09-17 17:57:59 +0000

    не спрацювало .зроби вже нарешті так як має бути.якщо потрібно то видали

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'hol-manager-v3';
-const CACHE_FILES = [
+const CACHE_NAME = 'hol-manager-cache-v1';
+const urlsToCache = [
   '/',
   '/login',
   '/manifest.json',
@@ -7,46 +7,40 @@ const CACHE_FILES = [
   '/icon/icon-512x512.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache and caching files');
-      return cache.addAll(CACHE_FILES);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
         })
       );
-    })
-  );
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response; // Return from cache
-      }
-      // Not in cache, fetch from network
-      return fetch(event.request).catch(() => {
-        // If network fails and it's a navigation request, return offline page
-        if (event.request.mode === 'navigate') {
-          return caches.match('/login');
-        }
-        // For other requests, just fail
-      });
     })
   );
 });
