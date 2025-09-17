3ecf8e00c06664d502353ec8b96a273eee665d87
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2025-09-17 16:13:25 +0000

    немає кнопки для інсталяції програми

--- public/sw.js
+++ public/sw.js
@@ -1,25 +1,30 @@
+// This is the service worker file.
+// It's a JavaScript file that runs in the background of your web app.
+// It can intercept network requests and cache files to make your app work offline.
 
 const CACHE_NAME = 'hol-manager-cache-v1';
 const urlsToCache = [
   '/',
   '/login',
-  '/manifest.json'
+  '/manifest.json',
+  '/favicon.ico',
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
+  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
+      .then((cache) => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
+      .then((response) => {
         // Cache hit - return response
         if (response) {
           return response;
@@ -32,9 +37,9 @@ self.addEventListener('fetch', event => {
         const fetchRequest = event.request.clone();
 
         return fetch(fetchRequest).then(
-          response => {
+          (response) => {
             // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
 
@@ -45,7 +50,7 @@ self.addEventListener('fetch', event => {
             const responseToCache = response.clone();
 
             caches.open(CACHE_NAME)
-              .then(cache => {
+              .then((cache) => {
                 cache.put(event.request, responseToCache);
               });
 
@@ -53,15 +58,16 @@ self.addEventListener('fetch', event => {
           }
         );
       })
-    );
+  );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
+
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
