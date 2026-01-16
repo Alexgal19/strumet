f2caa436925149a4cb85710acaf9b5bf59d0b45e
Oleksandr HOLIADYNETS <sanyagal191919@gmail.com>
2026-01-16 18:06:07 +0000

    Ja chce zeby po logowaniu pierwsza wkladka ktora ma otwierac sie ma byc

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,9 +1,8 @@
-
 'use client';
 import { redirect } from 'next/navigation';
 
 // This is the main entry point for the authenticated part of the app.
-// It redirects to the default view ('statystyki').
+// It redirects to the default view ('aktywni').
 export default function AppPage() {
-  redirect('/statystyki');
+  redirect('/aktywni');
 }
